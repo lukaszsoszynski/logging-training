c16317de037adad4ac64dd1b8a21ee0497192f69
Lukasz Soszynski <lsoszyn@gmail.com>
2016-10-09 17:10:18 +0200

    Http request logged

--- src/main/java/com/impaqgroup/training/logging/Application.java
+++ src/main/java/com/impaqgroup/training/logging/Application.java
@@ -1,17 +1,27 @@
 package com.impaqgroup.training.logging;
 
+import java.util.Collections;
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class Application {
+
+    private final static int FILTER_ORDER_COMMONS_REQUEST_LOGGING_FILTER = 1;
+    public static final String ALL_REQUEST_PATH = "/*";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -30,6 +40,26 @@ public class Application {
     @Bean
     public SecurityConfigurationAdapter securityConfigurationAdapter() {
         return new SecurityConfigurationAdapter();
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        CommonsRequestLoggingFilter filter = createLoggingFilter();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.setOrder(FILTER_ORDER_COMMONS_REQUEST_LOGGING_FILTER);
+        filterRegistrationBean.setMatchAfter(false);
+        filterRegistrationBean.setDispatcherTypes(EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
+        filterRegistrationBean.setUrlPatterns(Collections.singleton(ALL_REQUEST_PATH));
+        return filter;
+    }
+
+    private CommonsRequestLoggingFilter createLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        return filter;
     }
 
 }
